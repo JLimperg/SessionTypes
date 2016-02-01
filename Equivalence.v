15b0660f6392b880dacf31f3dbbf647eaba214b3
Jannis Limperg <jannis@limperg.de>
2016-02-01 21:36:22 +0100

    S ~ S' -> shape (substall S) = shape (substall S')

--- Equivalence.v
+++ Equivalence.v
@@ -1,4 +1,5 @@
-Require Import SessionTypes Substitution Tac.
+Require Import Env SessionTypes SessionTypesInd Shape ShapeFacts Substitution
+  SubstitutionFacts Tac Var Wellformed.
 
 Inductive sequiv_gen sequiv : Sty -> Sty -> Prop :=
 | sequiv_gen_end : sequiv_gen sequiv unit unit
@@ -29,7 +30,47 @@ CoInductive sequiv : Sty -> Sty -> Prop :=
 | sequiv_fold : forall S S', sequiv_gen sequiv S S' -> sequiv S S'.
 Hint Constructors sequiv.
 
+
 Lemma sequiv_coind : forall R,
   (forall S S', R S S' -> sequiv_gen R S S')
   -> (forall S S', R S S' -> sequiv S S').
 Proof. cofix CIH. introv H HR. apply H in HR. inverts HR; eauto. Qed.
+
+
+(* TODO beautify *)
+Lemma sequiv_shape :
+  forall S S',
+  ok_some S ->
+  ok_some S' ->
+  sequiv S S' ->
+  shape (substall S) = shape (substall S').
+Proof.
+  intros S S'. set (p := (S, S')).
+  apply well_founded_induction_type_2 with
+    (R := lt_Sty_mu_prefix2)
+    (P := fun S S' => ok_some S -> ok_some S' -> sequiv S S' ->
+     shape (substall S) = shape (substall S')).
+  apply lt_Sty_mu_prefix2_wf. clear S S' p.
+
+  intros S S' IH HokS HokS' Heq. inverts2 Heq; auto; decompose_ex HokS;
+    decompose_ex HokS'.
+
+    rename S0 into S. simpl. inversion HokS; subst.
+    rewrite shape_subst_substall_1 with (XS := env_add X XS); auto.
+    rewrite shape_subst_substall_2 with (XS := XS) (X := X) (R := mu X S); auto.
+    apply IH.
+      constructor. apply lt_Sty_mu_prefix_subst. eauto.
+      eexists. eapply subst_preserves_wellformedness; eauto.
+      eauto.
+      auto.
+
+    rename S'0 into S'. simpl. inversion HokS'; subst.
+    rewrite shape_subst_substall_1 with (XS := env_add X XS0); auto.
+    asserts_rewrite (shape (substall S') = shape (substall (subst X (mu X S') S'))).
+      apply shape_subst_substall_2 with (XS := XS0); auto.
+    apply IH.
+      constructor. apply lt_Sty_mu_prefix_subst. eauto.
+      eauto.
+      eexists. eapply subst_preserves_wellformedness; eauto.
+      auto.
+Qed.
