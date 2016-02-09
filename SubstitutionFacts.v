0623ca5e1ac43541ae3b7e588994ff5d991c3bd6
Jannis Limperg <jannis@limperg.de>
2016-02-09 01:18:58 +0100

    Some lemmas about cs/sequiv

--- SubstitutionFacts.v
+++ SubstitutionFacts.v
@@ -100,6 +100,22 @@ Proof.
 Qed.
 
 
+Lemma cs_ex :
+  forall S XS,
+  ok XS S ->
+  exists csS,
+  cs S csS.
+Proof.
+  induction S using (well_founded_ind lt_Sty_mu_prefix_wf). destruct S;
+    eauto with subst; introv Hok.
+  - assert (exists csS', cs (subst v (mu v S) S) csS').
+      pose proof Hok. inverts Hok.
+      eapply H;
+        eauto using subst_preserves_wellformedness, lt_Sty_mu_prefix_subst.
+    eauto with subst.
+Qed.
+
+
 Lemma cs_shape :
   forall S S' XS,
   ok XS S ->
@@ -118,10 +134,20 @@ Proof.
 Qed.
 
 
+Lemma cs_mu_absurd :
+  forall S XS X S',
+  ok XS S ->
+  cs S (mu X S') ->
+  False.
+Proof.
+  introv Hok Hcs. eapply cs_shape in Hcs; eauto.
+  decompose_or_auto; discriminate.
+Qed.
+
+
 (* TODO beautify. Perhaps use that sequiv is symmetric. *)
 Lemma cs_preserves_sequiv :
-  forall S S',
-  forall csS csS' XS XS',
+  forall S S' csS csS' XS XS',
   ok XS S ->
   ok XS' S' ->
   cs S csS ->
@@ -149,4 +175,84 @@ Proof.
         eauto using lt_Sty_mu_prefix_subst, subst_preserves_wellformedness
         with subst
     ].
+Qed.
+
+
+Lemma uncs_preserves_sequiv_l :
+  forall S S' csS csS' XS,
+  ok XS S ->
+  cs S csS ->
+  cs S' csS' ->
+  sequiv csS csS' ->
+  sequiv S csS'.
+Proof.
+  induction S using (well_founded_ind lt_Sty_mu_prefix_wf);
+    introv Hok Hcs Hcs' Heq; inverts1 Hcs; auto.
+  - do 2 constructor. pose proof Hok. inverts Hok.
+    eapply H with
+      (y := subst X (mu X S0) S0)
+      (S':= S')
+      (csS := csS);
+      eauto using lt_Sty_mu_prefix_subst, subst_preserves_wellformedness.
+Qed.
+
+
+(* TODO Avoid duplication between this and uncs_preserves_sequiv_l. *)
+Lemma uncs_preserves_sequiv_r :
+  forall S S' csS csS' XS',
+  ok XS' S' ->
+  cs S csS ->
+  cs S' csS' ->
+  sequiv csS csS' ->
+  sequiv csS S'.
+Proof.
+  induction S' using (well_founded_ind lt_Sty_mu_prefix_wf);
+    introv Hok' Hcs Hcs' Heq; inverts1 Hcs'; auto.
+  - do 2 constructor. pose proof Hok'. inverts Hok'.
+    eapply H with
+      (y := subst X (mu X S0) S0)
+      (csS' := csS');
+      eauto using lt_Sty_mu_prefix_subst, subst_preserves_wellformedness.
+Qed.
+
+
+Lemma cs_preserves_sequiv_l :
+  forall S S' csS XS XS',
+  ok XS S ->
+  ok XS' S' ->
+  cs S csS ->
+  sequiv S S' ->
+  sequiv csS S'.
+Proof.
+  intros. pose proof (cs_ex S') as HcsS'. edestruct HcsS'; eauto.
+  eapply uncs_preserves_sequiv_r with (S := S); eauto.
+  eapply cs_preserves_sequiv with (S := S) (S' := S'); eauto.
+Qed.
+
+
+Lemma cs_preserves_sequiv_r :
+  forall S S' csS' XS XS',
+  ok XS S ->
+  ok XS' S' ->
+  cs S' csS' ->
+  sequiv S S' ->
+  sequiv S csS'.
+Proof.
+  intros. pose proof (cs_ex S) as HcsS. edestruct HcsS; eauto.
+  eapply uncs_preserves_sequiv_l with (S' := S'); eauto.
+  eapply cs_preserves_sequiv with (S := S) (S' := S'); eauto.
+Qed.
+
+
+Lemma cs_preserves_wellformedness :
+  forall S csS,
+  cs S csS ->
+  wellformed S ->
+  wellformed csS.
+Proof.
+  induction S using (well_founded_ind lt_Sty_mu_prefix_wf);
+    introv Hcs Hwf; inverts Hcs; auto.
+  - pose proof Hwf. inverts Hwf. eapply H with (y := subst X (mu X S0) S0);
+      [eauto using lt_Sty_mu_prefix_subst|auto|].
+      apply subst_preserves_wellformedness; auto.
 Qed.
