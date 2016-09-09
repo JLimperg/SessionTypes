1d7b35c68b9b5f170ed7f76dd4a7f8342f54f6a2
Jannis Limperg <jannis@limperg.de>
2016-09-09 02:03:09 +0200

    Remove unused Sty induction principle
    StyInd had a definition of the construction order on session types in
    case we wanted to do structural induction/recursion in the style of
    well-founded induction/recursion. Turns out we don't.

--- src/StyInd.v
+++ src/StyInd.v
@@ -3,48 +3,6 @@ Require Import Arith.Wf_nat Relations.Relation_Operators
   Wellfounded.Lexicographic_Product.
 
 (*****************************************************************************)
-(* Construction order *)
-
-
-Inductive lt_Sty : Sty -> Sty -> Prop :=
-| lt_Sty_send :
-    forall S B, lt_Sty S (send B S)
-| lt_Sty_recv :
-    forall S B, lt_Sty S (recv B S)
-| lt_Sty_echoice_l :
-    forall S1 S2, lt_Sty S1 (echoice S1 S2)
-| lt_Sty_echoice_r :
-    forall S1 S2, lt_Sty S2 (echoice S1 S2)
-| lt_Sty_ichoice_l :
-    forall S1 S2, lt_Sty S1 (ichoice S1 S2)
-| lt_Sty_ichoice_r :
-    forall S1 S2, lt_Sty S2 (ichoice S1 S2)
-| lt_Sty_mu :
-    forall S X, lt_Sty S (mu X S)
-.
-
-
-Lemma lt_Sty_wf : well_founded lt_Sty.
-Proof. red. induction a; constructor; introv H; inverts H; auto. Defined.
-
-
-Definition lt_Sty2 : Sty * Sty -> Sty * Sty -> Prop
-  := syncprod lt_Sty lt_Sty.
-
-
-Lemma lt_Sty2_wf : well_founded lt_Sty2.
-Proof. apply wf_syncprod; apply lt_Sty_wf. Defined.
-
-
-Lemma Sty_ind2 :
-  forall (P : Sty -> Sty -> Prop),
-  (forall S S',
-    (forall T T', lt_Sty2 (T, T') (S, S') -> P T T') ->
-    P S S') ->
-  forall S S', P S S'.
-Proof. intro P. apply (well_founded_induction_type_2 P lt_Sty2_wf). Qed.
-
-(*****************************************************************************)
 (* Order by length of longest prefix consisting only of mu constructors *)
 
 
